f78e3eba1931b23f9b13d57db12ff0bb2a7cc7cf
Guy Marshall <guymarshall9@gmail.com>
2022-10-07 21:28:57 +0100

    Remove comments above program

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-/*
-There is a little one that comes to mind: if the Earth stopped in its orbit around the Sun, it would then
-accelerate under gravity until it hit the Sun. Using Newton's equation for universal gravitation, you could
-calculate the Earth's progress as it falls towards the Sun second by second.
-
-Yes, and for the acceleration (which is what you care about, not the force) you can remove the mass of the
-Earth from that equation
-
-It will be similar to your car acceleration problem.
-
-Work out the acceleration, a = -GM/(r²) . Take this as constant for 1s = delta-t .
-
-The change in velocity, delta-v = a × delta-t .
-
-The change in displacement (distance from the Sun) in 1s, delta-s = v × delta-t .
-
-The new displacement, s = s-old + delta-s .
-And similar to work out the new v.
-*/
-
 fn main() {
     const GRAVITATIONAL_CONSTANT: f64 = 0.000000000066743;
     const MASS_OF_SUN_KG: f64 = 1988470000000000000000000000000.0;
